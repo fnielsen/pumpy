d791922249492796828a71156e3400c2e1d211f9
Finn Aarup Nielsen <faan@dtu.dk>
2013-10-29 11:04:14 +0100

    header information.

--- pumpy/__init__.py
+++ pumpy/__init__.py
@@ -1,4 +1,15 @@
+# -*- coding: utf-8 -*-
 
+"""
+pumpy
+
+"""
+
+__title__ = "pumpy"
+__version__ = 0.1
+__author__ = "Finn Aarup Nielsen"
+__license__ = "GPLv3"
+__copyright__ = "DTU"
 
 from core import *
 
