935f1213819525941c292d4ea18ef4e11fd0bec9
fnielsen <fn@imm.dtu.dk>
2014-09-16 11:44:54 +0200

    Style fix.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "pumpy",
-    version = "0.0.1",
-    author = "Finn Aarup Nielsen",
-    author_email = "faan@dtu.dk",
-    description = ("An demonstration of pure Python matrix library"),
-    license = "GPL",
-    keywords = "example documentation tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
+    name="pumpy",
+    version="0.0.1",
+    author="Finn Aarup Nielsen",
+    author_email="faan@dtu.dk",
+    description=("An demonstration of pure Python matrix library"),
+    license="GPL",
+    keywords="example documentation tutorial",
+    url="http://packages.python.org/an_example_pypi_project",
     packages=['pumpy', 'pumpy.core', 'pumpy.core.test', 'examples'],
     long_description=read('README.md'),
     classifiers=[
