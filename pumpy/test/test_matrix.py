12fe8a05faf8e20126b43c8f3b4905fb219b9725
fnielsen <fn@imm.dtu.dk>
2014-09-16 11:54:14 +0200

    Moved from pumpy/core/test.

---
+++ pumpy/test/test_matrix.py
@@ -0,0 +1,21 @@
+
+
+from ..multiarray import Matrix
+
+
+def test_getitem():
+    m = Matrix([[1, 2], [3, 4]])
+    assert m[0, 1] == 2
+    assert m[1, 1] == 4
+
+
+def test_transpose():
+    m = Matrix([[1, 2], [3, 4]])
+    mt = m.transpose()
+    assert m[0, 1] == mt[1, 0]
+
+
+def test_T():
+    m = Matrix([[1, 2], [3, 4]])
+    mt = m.T
+    assert m[0, 1] == mt[1, 0]
