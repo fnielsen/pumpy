8a48817eab2046f22f48c7de542d4c81abfe8559
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-10-09 22:43:37 +0200

    Added a few more tests.

--- pumpy/test/test_matrix.py
+++ pumpy/test/test_matrix.py
@@ -1,6 +1,6 @@
 
 
-from ..multiarray import Matrix
+from ..matrix import Matrix
 
 
 def test_getitem():
@@ -12,7 +12,9 @@ def test_getitem():
 def test_transpose():
     m = Matrix([[1, 2], [3, 4]])
     mt = m.transpose()
+    assert mt[0, 0] == 1
     assert m[0, 1] == mt[1, 0]
+    assert mt[1, 0] == 2
 
 
 def test_T():
