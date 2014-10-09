af1d9a5882a666ccbf5c6cb882fa849b68c0b5c7
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-10-09 22:58:39 +0200

    Corrected a number of fields that was not set correctly (packages, keywords, ...)

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 import os
 from setuptools import setup
+from codecs import open
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 
 setup(
     name="pumpy",
     version="0.0.1",
     author="Finn Aarup Nielsen",
     author_email="faan@dtu.dk",
-    description=("An demonstration of pure Python matrix library"),
+    description="A demonstration of pure Python matrix library",
     license="GPL",
-    keywords="example documentation tutorial",
-    url="http://packages.python.org/an_example_pypi_project",
-    packages=['pumpy', 'pumpy.core', 'pumpy.core.test', 'examples'],
-    long_description=read('README.md'),
+    keywords="matrix numerical",
+    url="https://github.com/fnielsen/pumpy",
+    packages=['pumpy', 'pumpy.test', 'examples'],
+    long_description=readme,
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
