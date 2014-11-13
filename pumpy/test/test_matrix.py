cf2415d6e791cf5304b8d7a61fd094b7ee01d962
fnielsen <fn@imm.dtu.dk>
2014-11-13 15:50:52 +0100

    Docstrings.

--- pumpy/test/test_matrix.py
+++ pumpy/test/test_matrix.py
@@ -1,21 +1,25 @@
+"""Testing functions for matrix module."""
 
 
 from ..matrix import Matrix
 
 
 def test_getitem():
+    """Test matrix getitem."""
     m = Matrix([[1, 2], [3, 4]])
     assert m[0, 1] == 2
     assert m[1, 1] == 4
 
 
 def test_transpose():
+    """Test Matrix transpose method."""
     m = Matrix([[1, 2], [3, 4]])
     mt = m.transpose()
     assert m[0, 1] == mt[1, 0]
 
 
 def test_T():
+    """Test Matrix T propery."""
     m = Matrix([[1, 2], [3, 4]])
     mt = m.T
     assert m[0, 1] == mt[1, 0]
