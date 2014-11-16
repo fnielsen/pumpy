dca2669a6d9c7815a701c0da460fc57eb6591bdf
Finn Årup Nielsen <fn@imm.dtu.dk>
2014-11-16 22:29:35 +0100

    Added test functions.

--- pumpy/test/test_matrix.py
+++ pumpy/test/test_matrix.py
@@ -1,7 +1,14 @@
 """Testing functions for matrix module."""
 
 
-from ..matrix import Matrix
+from .. import Matrix
+
+
+def test_abs():
+    matrix = Matrix([[-2, 3]])
+    abs_matrix = abs(matrix)
+    assert abs_matrix[0, 0] == 2
+    assert abs_matrix[0, 1] == 3
 
 
 def test_getitem():
@@ -9,6 +16,18 @@ def test_getitem():
     m = Matrix([[1, 2], [3, 4]])
     assert m[0, 1] == 2
     assert m[1, 1] == 4
+
+
+def test_setitem():
+    matrix = Matrix([[1, 2]])
+    matrix[0, 1] = 3
+    assert matrix[0, 1] == 3
+    assert matrix[0, 0] == 1
+
+
+def test_shape():
+    matrix = Matrix([[1, 2], [3, 4], [5, 6]])
+    assert matrix.shape == (3, 2)
 
 
 def test_transpose():
